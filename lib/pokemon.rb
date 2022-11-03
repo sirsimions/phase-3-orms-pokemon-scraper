869e1b7d7c7d3873b7138c338d034cce1272a012
Simion <simion.okola@student.moringaschool.com>
2022-11-03 22:33:59 +0300

    Completed assignment

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:,type:,db:)
+        @id=id
+        @name=name
+        @type=type
+        @db=db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)", name,type)
+    end
+
+    def self.find(id, db)
+        pokemon_pull=db.execute("SELECT * FROM pokemon WHERE id=?",id)[0]
+
+        self.new(id:pokemon_pull[0],name:pokemon_pull[1],type:pokemon_pull[2],db:pokemon_pull[3])
+
+    end
 end
